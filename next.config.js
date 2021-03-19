c91d45426341385b1a79c37b5b1c3c8fe3764d61
Karthikvdk <52970201+Karthikvdk@users.noreply.github.com>
2021-03-19 15:27:45 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-module.exports = {
-    env: {
-        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY : "pk_test_51IWHbnCIaWRb17DwLyEX2EVgDsqMfnVUvcRa1ONhpKFkzV0uDcl1WhSwmA7Y9mAjHCY4XcXwryVIfd7WczPzGQhO00hqsTcI02",    
-    },
-  }
+require('dotenv').config({
+    path: './.env.local'
+  });
